73a8a879b6e453de68c26b7d3691ada38888e53d
Aditya Kulkarni <adityag.kulkarni17893@aissmsioit.org>
2026-07-07 10:24:13 +0530

    fix CORS remove wildcard options

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
-app.options('*', cors())
 
 // Security
 app.use(helmet())
