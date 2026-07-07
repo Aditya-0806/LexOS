24ce32fa5c8fd8bfe3f95814d4373d22f99248e8
Aditya Kulkarni <adityag.kulkarni17893@aissmsioit.org>
2026-07-07 09:15:32 +0530

    update API URL to production backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
-});
+  baseURL: 'https://lexos-backend-ek2i.onrender.com/api'
+})
 
 // Automatically attach token to every request
 API.interceptors.request.use((req) => {
